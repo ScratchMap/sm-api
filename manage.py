b1582ecff9835bbc0686b4024bfd68c76559911d
Alexandr Kravchenko <drhihi@ukr.net>
2017-07-10 11:17:30 +0300

    test

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 from app import create_app
+from flask import current_app
 
 manager = Manager(create_app)
 manager.add_option('-c', '--config', dest='config', required=False)
@@ -10,5 +11,5 @@ manager.add_command('db', MigrateCommand)
 # manager.add_command('test', PytestCommand)
 
 if __name__ == '__main__':
-    print(manager._options)
+    print(current_app)
     manager.run()
